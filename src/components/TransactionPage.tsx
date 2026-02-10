8df8404379bbf1ad13878fbe440547ee4d1c8fd2
JulienBCHZ <julienbouchez@icloud.com>
2026-02-10 15:57:44 +0100

    add input component & design for data presentation

--- src/components/TransactionPage.tsx
+++ src/components/TransactionPage.tsx
@@ -3,6 +3,7 @@ import Input from "./ui/Input";
 import { useState } from "react";
 
 // icons //
+import { FiSearch } from "react-icons/fi";
 
 //   type,
 //   placeholder,
@@ -20,22 +21,26 @@ const TransactionsPage = () => {
   };
 
   return (
-    <div className="min-h-[calc(100dvh-56px)] flex flex-col items-center gap-5 pt-10">
+    <div className="min-h-[calc(100dvh-56px)] flex flex-col relative items-center gap-5 pt-10 ">
       <div className="font-bold">
         <h1 className="text-xl text-brand-800">
           Lydia <span className="text-brand-500">paiement history</span>
         </h1>
       </div>
-      <div className="sticky w-80">
-        <Input
-          type="text"
-          placeholder="Search payment..."
-          name="search"
-          value={search}
-          onChange={handleSearch}
-          className="bg-brand-100 pl-10"
-        />
-      </div>
+      <section className="sticky w-80 top-0">
+        <div className="relative">
+          <FiSearch className="absolute top-2 left-2.5" />
+          <Input
+            type="text"
+            placeholder="Search payment..."
+            name="search"
+            value={search}
+            onChange={handleSearch}
+            className="bg-brand-100 pl-8.5"
+          />
+        </div>
+      </section>
+      <section className="min-h-300 bg-brand-500"></section>
     </div>
   );
 };
